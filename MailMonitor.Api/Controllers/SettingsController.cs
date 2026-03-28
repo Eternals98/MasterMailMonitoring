a24d7164b23f388062470a4d03994909a830497a
Eternals98 <94234702+Eternals98@users.noreply.github.com>
2026-03-28 18:56:03 -0500

    Add API endpoint for getting settings

--- MailMonitor.Api/Controllers/SettingsController.cs
+++ MailMonitor.Api/Controllers/SettingsController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using MailMonitor.Api.Contracts.Settings;
+using MailMonitor.Application.Abstractions.Configuration;
+using Microsoft.AspNetCore.Mvc;
 
-namespace MailMonitor.Api.Controllers
+namespace MailMonitor.Api.Controllers;
+
+[ApiController]
+[Route("api/settings")]
+public sealed class SettingsController : ControllerBase
 {
-    public class SettingsController : Controller
+    private static readonly char[] KeywordsSeparators = [',', ';', '\n', '\r'];
+
+    private readonly IConfigurationService _configurationService;
+
+    public SettingsController(IConfigurationService configurationService)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _configurationService = configurationService;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(SettingsResponse), StatusCodes.Status200OK)]
+    public async Task<ActionResult<SettingsResponse>> GetAsync(CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var settings = await _configurationService.GetSettingsAsync();
+
+        var response = new SettingsResponse(
+            settings.BaseStorageFolder,
+            settings.MailSubjectKeywords
+                .Split(KeywordsSeparators, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries),
+            settings.ProcessingTag);
+
+        return Ok(response);
     }
 }
