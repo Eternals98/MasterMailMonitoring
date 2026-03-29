cfc42da33da401712583f1653b988a70e1906655
Eternals98 <94234702+Eternals98@users.noreply.github.com>
2026-03-28 23:58:22 -0500

    Add PUT /api/companies/{id} update endpoint

---
+++ MailMonitor.Api/Contracts/Companies/UpdateCompanyRequest.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MailMonitor.Api.Contracts.Companies;
+
+public sealed class UpdateCompanyRequest
+{
+    [Required]
+    public Guid Id { get; init; }
+
+    [Required]
+    public string Name { get; init; } = string.Empty;
+
+    [Required]
+    public string Mail { get; init; } = string.Empty;
+
+    public string StartFrom { get; init; } = string.Empty;
+
+    public IReadOnlyCollection<string> MailBox { get; init; } = [];
+
+    public IReadOnlyCollection<string> FileTypes { get; init; } = [];
+
+    public IReadOnlyCollection<string> AttachmentKeywords { get; init; } = [];
+
+    [Required]
+    public string StorageFolder { get; init; } = string.Empty;
+
+    [Required]
+    public string ReportOutputFolder { get; init; } = string.Empty;
+
+    [Required]
+    public string ProcessingTag { get; init; } = string.Empty;
+}
